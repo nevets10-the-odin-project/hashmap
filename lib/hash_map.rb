9f32fe4b7d89ea1f15fccbd18e134fd4804e2bff
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-30 17:48:19 -0800

    Create has_map.rb with HashMap class

---
+++ lib/hash_map.rb
@@ -0,0 +1,12 @@
+class HashMap
+  attr_accessor :load_factor, :capacity
+
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+
+    hash_code
+  end
+end
